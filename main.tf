7d65f0ed410f7b3dfdf9a771fef1b582f97697a9
Lokmanavsar <avsarlokman@gmail.com>
2025-02-07 20:54:05 -0500

    Step 3: Defined Terraform required_version and providers (azurerm and cloudinit)

---
+++ main.tf
@@ -0,0 +1,24 @@
+# Configure the Terraform runtime requirements.
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+}
+
+# Define providers and their config params
+provider "azurerm" {
+  features {}
+}
+
+provider "cloudinit" {
+  # Configuration options (varsayılan değerler yeterli)
+}
