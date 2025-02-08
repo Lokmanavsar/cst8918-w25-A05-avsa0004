eef8cc0a7133e02ad178362bf7ea6f32e2eb1c45
Lokmanavsar <avsarlokman@gmail.com>
2025-02-07 21:04:57 -0500

    Step 4.1: Added variables for labelPrefix, region, and admin_username

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   required_providers {
+    # Azure Resource Manager provider and version
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
@@ -16,9 +17,27 @@ terraform {
 
 # Define providers and their config params
 provider "azurerm" {
+  # Leave the features block empty to accept all defaults
   features {}
 }
 
 provider "cloudinit" {
-  # Configuration options (varsayılan değerler yeterli)
+  # Configuration options
+}
+
+variable "labelPrefix" {
+  description = "A prefix for all resource names, typically your college username"
+  type        = string
+}
+
+variable "region" {
+  description = "Azure region to deploy resources"
+  type        = string
+  default     = "canadacentral"
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureadmin"
 }
