34369e4b2e1abf5c20dcc0384b455cc7d88566af
Lokmanavsar <avsarlokman@gmail.com>
2025-02-07 21:41:41 -0500

    Step 4.5: Created Subnet within the VNet

--- main.tf
+++ main.tf
@@ -62,6 +62,14 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.labelPrefix}-A05-Subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.labelPrefix}-A05-NSG"
   location            = var.region
